2ac2cebc9cbc897b49c7256f01034f4133318257
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-05 18:29:52 +0000

    Remove arrays and use an object to keep track of state.

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
-
 function newTask(title, description) {
-  taskTitles.push(title);
-  taskDescriptions.push(description);
-  taskComplete.push(false);
+  const task = {
+    title,
+    description,
+    complete: false
+  };
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+const task1 = newTask('Clean Cat Litter', 'Take all the 💩 out of the litter box');
+const task2 = newTask('Do laundry', '🙃');
+const tasks = [task1, task2];
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+console.log(tasks);
 
-// DRIVER CODE BELOW
+// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(taskIndex) {
+//   taskComplete[taskIndex] = true;
+// }
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+// // Print the state of a task to the console in a nice readable way
+// function logTaskState(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// }
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+// // DRIVER CODE BELOW
+
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed
